c635961d2561be2ce56dbdfdca606a8b5ef59c2f
Matthew Feng <mattfeng@mit.edu>
2022-03-16 12:19:33 -0400

    rename

---
+++ ace-web/gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: `ACE Group`,
+    description: `The ACE Group brings together students with different skillsets to accelerate the Convergence Revolution.`,
+    author: `@mattfeng`,
+    siteUrl: `https://aceresearch.github.io`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
