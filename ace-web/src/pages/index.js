8d111309d5636c75d8738b51266f512094f30891
Matthew Feng <mattfeng@mit.edu>
2022-03-17 12:59:14 -0400

    remove header

--- ace-web/src/pages/index.js
+++ ace-web/src/pages/index.js
@@ -3,15 +3,6 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import * as styles from "../styles/index.module.scss"
 
-const IndexPage = () => (
-  <>
-    <StaticImage
-      src="../images/ace.png"
-      alt="ace logo"
-      width={150}
-      height={150}
-    />
-  </>
-)
+const IndexPage = () => <></>
 
 export default IndexPage
