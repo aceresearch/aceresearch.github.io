3f7752098e7d1144ac138ef3093d6e41f27a0157
Matthew Feng <mattfeng@mit.edu>
2022-03-20 00:35:08 -0400

    add images

--- ace-web/src/pages/problems.js
+++ ace-web/src/pages/problems.js
@@ -1,23 +1,37 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 import * as styles from "../styles/problems.module.scss"
 
-const ProblemsPage = () => (
-  <Layout>
-    <div className={styles.problemsContainer}>
-      <ul className={styles.problemsList}>
-        <li>bioremediation</li>
-        <li>cancer</li>
-        <li>carbon capture</li>
-        <li>clean energy</li>
-        <li>clean manufacturing</li>
-        <li>education</li>
-        <li>microbiome</li>
-        <li>neurodegenerative diseases</li>
-      </ul>
-    </div>
-  </Layout>
-)
+const ProblemsPage = () => {
+  const [image, setImage] = useState(null)
+
+  const backgroundImage = {
+    backgroundImage: `url(/${image}.jpg)`,
+    backgroundSize: `600px`,
+  }
+
+  return (
+    <Layout>
+      <div className={styles.problemsContainer}>
+        <ul className={styles.problemsList}>
+          <li onClick={() => setImage("coral")}>bioremediation</li>
+          <li onClick={() => setImage("cancer")}>cancer</li>
+          <li onClick={() => setImage("trees")}>carbon capture</li>
+          <li onClick={() => setImage("windmills")}>clean energy</li>
+          <li onClick={() => setImage("factory")}>clean manufacturing</li>
+          <li onClick={() => setImage("library")}>education</li>
+          <li onClick={() => setImage("gut")}>microbiome</li>
+          <li onClick={() => setImage("neurons")}>
+            neurodegenerative diseases
+          </li>
+        </ul>
+
+        <div className={styles.imageContainer} style={backgroundImage}></div>
+      </div>
+    </Layout>
+  )
+}
 
 export default ProblemsPage
